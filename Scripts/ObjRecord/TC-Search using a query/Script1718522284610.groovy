2c457cbb7993da426a5b649f4cfc4704d1b79938
nasmahna <nasmahna@localhost>
2024-06-16 14:44:19 +0700

    add object record repository and test cases

---
+++ Scripts/ObjRecord/TC-Search using a query/Script1718522284610.groovy
@@ -0,0 +1,47 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('/Users/nasmahna/Documents/Katalon-GITS/katalon-mobile-p2024/APK/APIDemos.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/ObjRecord/App-Search/android.widget.TextView - App'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ObjRecord/App-Search/android.widget.TextView - Search'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ObjRecord/App-Search/android.widget.TextView - Query Search Results'), 0)
+
+Mobile.getText(findTestObject('Object Repository/ObjRecord/App-Search/android.widget.TextView - This activity accepts query strings via the ACTION_SEARCH intent. In a full implementation, you would use the query string to select results from your data source, and present a list of those results to the user'), 
+    0)
+
+Mobile.getText(findTestObject('Object Repository/ObjRecord/App-Search/android.widget.TextView - Query String'), 0)
+
+Mobile.getText(findTestObject('Object Repository/ObjRecord/App-Search/android.widget.TextView - Query App Data'), 0)
+
+Mobile.getText(findTestObject('Object Repository/ObjRecord/App-Search/android.widget.TextView - Activity Method'), 0)
+
+Mobile.getText(findTestObject('Object Repository/ObjRecord/App-Search/android.widget.TextView - onCreate(), but no ACTION_SEARCH intent'), 
+    0)
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
+Mobile.closeApplication()
+

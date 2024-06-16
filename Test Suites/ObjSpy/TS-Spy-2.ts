5c451ad7f900d879b3c8578e88cf78b8ec62f1f6
nasmahna <nasmahna@192.168.1.11>
2024-06-16 16:59:45 +0700

    add test suites for obj record and obj spy

---
+++ Test Suites/ObjSpy/TS-Spy-2.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-Spy-2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b12aabaf-388b-4b38-832a-1f4d6c7765d1</testSuiteGuid>
+   <testCaseLink>
+      <guid>e74eec28-e3d2-4094-a76e-58ca9922cc13</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ObjSpy/TC-notify using incoming message and with text</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>93901c86-c984-4599-9f5d-e0dc07468f6b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ObjSpy/TC-set alarm and stop</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
